3cdf3b9e2c0feb2379f31cee6ce98020a65a3cb9
bahar <jony.bgc08@gmail.com>
2017-04-09 13:45:12 +0600

    package created

---
+++ src/main/java/univ/savemother/SaveMotherAndChildrenApplication.java
@@ -0,0 +1,12 @@
+package univ.savemother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveMotherAndChildrenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaveMotherAndChildrenApplication.class, args);
+	}
+}
---
+++ src/test/java/univ/savemother/SaveMotherAndChildrenApplicationTests.java
@@ -0,0 +1,16 @@
+package univ.savemother;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SaveMotherAndChildrenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
