b1d079d1c8deefc16f142d3c67ddff3ca31e0da7
Bahar <Bahar>
2017-04-18 20:14:02 +0600

    initial commit

---
+++ src/main/java/univ/savemother/utils/Response.java
@@ -0,0 +1,22 @@
+package univ.savemother.utils;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Response<T> {
+	@JsonInclude(JsonInclude.Include.ALWAYS)
+	private String status;
+	@JsonInclude(JsonInclude.Include.ALWAYS)
+	private String statusCode;
+	@JsonInclude(JsonInclude.Include.ALWAYS)
+	private int count;
+	@JsonInclude(JsonInclude.Include.ALWAYS)
+	private String type;
+	@JsonInclude(JsonInclude.Include.ALWAYS)
+	private T results;
+
+}
---
+++ src/main/java/univ/savemother/utils/UrlConstants.java
@@ -0,0 +1,10 @@
+package univ.savemother.utils;
+
+public class UrlConstants {
+	private UrlConstants(){
+		
+	}
+	public static final String VERSION = "/v1";
+	public static final String MOVIE = VERSION+"/movies";
+	public static final String MOVIE_ID = VERSION+MOVIE+"/{id}";
+}
